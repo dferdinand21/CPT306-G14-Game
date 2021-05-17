9ad8faea7090930c91f30c5ae52f646caa70eac4
David <dferdinand21@gmail.com>
2021-05-17 21:40:20 +0700

    Created 1st Person Tester
    Created a scene for testing in First Person View

---
+++ DemoToStart/Assets/Scripts/ThrowObject.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using System.Collections;
+
+public class ThrowObject : MonoBehaviour
+{
+    public Transform player;
+    public Transform playerCam;
+    public float throwForce = 10;
+    public bool hasPlayer = false;
+    public bool beingCarried = false;
+    public int dmg;
+    private bool touched = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float dist = Vector3.Distance(gameObject.transform.position, player.position);
+        if (dist <= 2.5f)
+        {
+            hasPlayer = true;
+        }
+        else
+        {
+            hasPlayer = false;
+        }
+        if (hasPlayer && Input.GetButtonDown("Use"))
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            transform.parent = playerCam;
+            beingCarried = true;
+        }
+        if (beingCarried)
+        {
+            if (touched)
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                touched = false;
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce);
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        if (beingCarried)
+        {
+            touched = true;
+        }
+    }
+}
