6a233d270f0ea896ecac0761b369d205c364d80b
David <dferdinand21@gmail.com>
2021-05-30 16:05:13 +0700

    Updated Countdown Script
    Updated the timer script, so that it counts down instead of up, and some minor tweaks on the FPtest Scene

--- DemoToStart/Assets/Scripts/HUDScript.cs
+++ DemoToStart/Assets/Scripts/HUDScript.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 
 public class HUDScript : MonoBehaviour
 {
-    float time;
+    float startTime = 600f;
+    float time = 0f;
     Text timeText;
 
     // Start is called before the first frame update
     void Start()
     {
+        time = startTime;
         timeText = GameObject.Find("Timer").GetComponent<Text>();
 
 
@@ -19,9 +21,21 @@ public class HUDScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime * 1;
+        time -= 1 * Time.deltaTime;
+        if (time <= 0)
+        {
+            time = 0;
+        }
         string minutes = Mathf.Floor((time % 3600) / 60).ToString("00");
-        string seconds = (time % 60).ToString("00");
+        string seconds = Mathf.Floor((time % 60)).ToString("00");
         timeText.text = minutes + ":" + seconds;
+    }
+
+    string timeFloattoString(float t)
+    {
+        string minutes = Mathf.Floor((t % 3600) / 60).ToString("00");
+        string seconds = Mathf.Floor((t % 60)).ToString("00");
+        string strTime = minutes + ":" + seconds;
+        return strTime;
     }
 }
