1ffe2a2effde7034a09022fd64dc37038acb9ac6
David <dferdinand21@gmail.com>
2021-06-05 18:34:37 +0700

    Added function to go to math scene on TTL

---
+++ DemoToStart/Assets/_Scripts/LeaveRoomKeypadControls.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaveRoomKeypadControls : MonoBehaviour
+{
+    public GameObject leaveRoomCanvas;
+    public string password;
+    public int passwordLimit;
+    public Text passwordText;
+
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip correctSound;
+    public AudioClip wrongSound;
+
+    private void Start()
+    {
+        passwordText.text = "";
+        leaveRoomCanvas.SetActive(false);
+    }
+
+    public void PasswordEntry(string number)
+    {
+        if (number == "Clear")
+        {
+            Clear();
+            return;
+        }
+        else if (number == "Enter")
+        {
+            Enter();
+            return;
+        }
+
+        int length = passwordText.text.ToString().Length;
+        if (length < passwordLimit)
+        {
+            passwordText.text = passwordText.text + number;
+        }
+    }
+
+    public void Clear()
+    {
+        passwordText.text = "";
+        passwordText.color = Color.white;
+    }
+
+    private void Enter()
+    {
+        if (passwordText.text == password)
+        {
+            //door.lockedByPassword = false;
+            leaveRoomCanvas.SetActive(true);
+
+            if (audioSource != null)
+                audioSource.PlayOneShot(correctSound);
+
+            passwordText.color = Color.green;
+            StartCoroutine(waitAndClear());
+        }
+        else
+        {
+            if (audioSource != null)
+                audioSource.PlayOneShot(wrongSound);
+
+            passwordText.color = Color.red;
+            StartCoroutine(waitAndClear());
+        }
+    }
+
+    IEnumerator waitAndClear()
+    {
+        yield return new WaitForSeconds(0.75f);
+        Clear();
+    }
+
+}
